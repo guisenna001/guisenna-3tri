8c74b9cfce34e3cb81d3f0866618cc8cbd424bde
guisenna001 <142246411+guisenna001@users.noreply.github.com>
2024-09-26 01:54:53 -0300

    Add files via upload

---
+++ alura-3-trimestre-senna-main/alura-3-trimestre-senna-main/senna/perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Matemática',
+    'O resolva a equação 2X+39=456, e descubra o valor do X ?',
+    'O valor do X:208,5'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se escreve "Ela mora sozinha" em Inglês?',
+    'Ela mora sozinha é "She lives alone"'
+)
+
+criaCarta(
+    'Matemática',
+    'Qual é a raiz quadrada de 25?',
+    'Raiz quadrada de 25 é: 5',
+    )
+criaCarta(
+    'Português',
+    'O que é Conto?',
+'É uma narrativa curta que, em geral, apresenta apenas um conflito',
+    )
+    
+    
